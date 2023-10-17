1dcb460e8c7ee0aeb043a9cb907783949227fab6
krychkarol <krych.karol.93@gmail.com>
2023-10-17 16:35:41 +0200

    navbar component

--- src/pages/navbar/navbar.jsx
+++ src/pages/navbar/navbar.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../navbar/_navbar.scss'
 
 const Navbar = () => {
-  return (
-    <div>navbar</div>
-  )
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const menuToggler = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+
+
+    return (
+        <section className="my-navbar container-fluid">
+            <div className="name f-special">krychkarol</div>
+            <div className="menu-desktop d-none d-lg-block">
+                <ul className="f-bold">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className="menu-mobile d-lg-none">
+                <div className={!isMenuOpen ? "hamburger-icon" : "hamburger-icon is-open"} onClick={menuToggler}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            <div className={!isMenuOpen ? "menu-mobile-dropdown d-lg-none" : "menu-mobile-dropdown is-open d-lg-none"}>
+                <ul className="f-bold">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+
+        </section>
+    )
 }
 
 export default Navbar
