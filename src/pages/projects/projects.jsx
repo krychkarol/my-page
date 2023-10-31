a50af6caf0d88e82cc6fd59a90c137007c1a68cb
krychkarol <krych.karol.93@gmail.com>
2023-10-31 15:49:44 +0100

    projects section

--- src/pages/projects/projects.jsx
+++ src/pages/projects/projects.jsx
@@ -15,7 +15,9 @@ const Projects = () => {
             {data.map(project => (
                 <div className="project" key={project.name}>
                     <div className="photo">
-                        <img src={project.img} alt="brodacz" />
+                        <a href={project.live} target='_blank' rel="noreferrer">
+                            <img src={project.img} alt="brodacz" />
+                        </a>
                     </div>
                     <div className="text">
                         <h2 className="f-b-bold">
@@ -44,39 +46,9 @@ const Projects = () => {
                     </div>
                 </div>
             ))}
-            {/* map */}
-            {/* <div className="project">
-                <div className="photo">
-                    <img src={Brodacz} alt="brodacz" />
-                </div>
-                <div className="text">
-                    <h2 className="f-b-bold">Brodacz</h2>
-                    <h3 className="f-bold">
-                        Full stack E-Commerce application with login / register users,
-                        shopping cart, products search, payment method and an admin panel
-                        that allows you to manage products,orders, users, categories or
-                        even a carousel.
-                    </h3>
-                    <span className="f-bold">
-                        React, Node, Express, MongoDB, Redux, JWT, Stripe, Firebase, MUI, RWD, RestAPI
-                    </span>
-                    <div className="links">
-                        <a href="https://github.com/krychkarol/brodacz-ecommerce-store" target='_blank' rel="noreferrer">
-                            <h3 className="f-bold">
-                                Code
-                                <FontAwesomeIcon icon={faCode} className="icon" />
-                            </h3>
-                        </a>
-                        <a href="https://brodacz.krychkarol.pl/" target='_blank' rel="noreferrer">
-                            <h3 className="f-bold">
-                                Live
-                                <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="icon" />
-                            </h3>
-                        </a>
-                    </div>
-                </div>
-            </div> */}
-
+            <div className="more">
+                <span className="f-bold">To see more of my work, visit my <a href="https://github.com/krychkarol" target='_blank' rel="noreferrer">github</a> profile.</span>
+            </div>
         </section>
     )
 }
--- src/pages/projects/projectsData.js
+++ src/pages/projects/projectsData.js
@@ -1,21 +1,22 @@
 import brodacz from '../../assets/images/brodacz.png'
+import montana from '../../assets/images/montanaphibrows.png'
 
 var data = [
     {
         name: "Brodacz",
-        dsc: "Full stack E-Commerce application with login / register users, shopping cart, products search, payment method and an admin panel that allows you to manage products, orders, users, categories or even a carousel.",
-        tech: "React, Node, Express, MongoDB, Redux, JWT, Stripe, Firebase, MUI, RWD, RestAPI",
+        dsc: "Full stack E-Commerce application with login / register users, shopping cart, products search, payment method and an admin panel that allows you to manage products, categories, orders and users.",
+        tech: "React, Node, Express, MongoDB, Redux, JWT, Stripe, Firebase",
         git: "https://github.com/krychkarol/brodacz-ecommerce-store",
         live: "https://brodacz.krychkarol.pl/",
         img: brodacz
     },
     {
-        name: "Brodacz1",
-        dsc: "Full stack E-Commerce application with login / register users, shopping cart, products search, payment method and an admin panel that allows you to manage products, orders, users, categories or even a carousel.",
-        tech: "React, Node, Express, MongoDB, Redux, JWT, Stripe, Firebase, MUI, RWD, RestAPI",
-        git: "https://github.com/krychkarol/brodacz-ecommerce-store",
-        live: "https://brodacz.krychkarol.pl/",
-        img: brodacz
+        name: "Montana PhiBrows",
+        dsc: "Beauty business card website, providing basic means of contact, service pricing and info section.",
+        tech: "Angular, Bootstrap",
+        git: "https://github.com/PawelMaslak/MontanaPhiBrows.Frontend",
+        live: "https://montanaphibrows.com/",
+        img: montana
     },
 ]
 
